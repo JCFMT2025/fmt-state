58dbe08220adece591a347d2659cd7dcf0a2fe39
JCFMT2025 <jamesrobertsoncurrie@yahoo.co.uk>
2025-05-04 22:48:05 +0100

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,14 +1,21 @@
 # START_LOADER
 
 import json
+import urllib.request
 
 with open("fmt-state.json", "r") as f:
     state = json.load(f)
 
 # START_SPLIT_STATE_OVERRIDE
 if state.get("metadata", {}).get("split_state_enabled"):
-    with open(state["metadata"]["core_state_path"], "r") as f:
-        state = json.load(f)
+    core_path = state["metadata"]["core_state_path"]
+
+    if core_path.startswith("http"):
+        with urllib.request.urlopen(core_path) as response:
+            state = json.load(response)
+    else:
+        with open(core_path, "r") as f:
+            state = json.load(f)
 # END_SPLIT_STATE_OVERRIDE
 
 print("Current version:", state["metadata"]["version"])
