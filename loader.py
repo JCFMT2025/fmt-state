08a55205976f25a389cc2f618195003dd343382d
JCFMT2025 <jamesrobertsoncurrie@yahoo.co.uk>
2025-05-04 22:15:16 +0100

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,15 +1,15 @@
 # START_LOADER
 
+import json
+
+with open("fmt-state.json", "r") as f:
+    state = json.load(f)
+
 # START_SPLIT_STATE_OVERRIDE
 if state.get("metadata", {}).get("split_state_enabled"):
     with open(state["metadata"]["core_state_path"], "r") as f:
         state = json.load(f)
 # END_SPLIT_STATE_OVERRIDE
-
-import json
-
-with open("fmt-state.json", "r") as f:
-    state = json.load(f)
 
 print("Current version:", state["metadata"]["version"])
 print("Full market scan enabled:", state.get("full_market_scan_enabled"))
