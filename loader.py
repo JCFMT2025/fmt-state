4db121b5e185a6e0a6559b99b2fac3b33fe7bd10
JCFMT2025 <jamesrobertsoncurrie@yahoo.co.uk>
2025-05-04 22:09:29 +0100

    Create loader.py

---
+++ loader.py
@@ -0,0 +1,8 @@
+# START_LOADER
+import json
+
+with open("fmt-state.json", "r") as f:
+    state = json.load(f)
+
+print("Current version:", state["metadata"]["version"])
+print("Full market scan enabled:", state.get("full_market_scan_enabled"))
